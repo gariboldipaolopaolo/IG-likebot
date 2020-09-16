6ee0a43ae1c02a7c8d975d819f188f8a61f468ae
gariboldipaolo <paolo.gariboldi@student.supsi.ch>
2020-09-16 11:36:56 +0200

    switch di lingua e aggiunti commenti

--- igBot.js
+++ igBot.js
@@ -1,7 +1,10 @@
-const next = document.querySelector('a.coreSpriteRightPaginationArrow');//seleziono il bottone prossimo post
-
+/**
+ * Click event on next and like button
+ * @param {decimal} random random time
+ */
 function likePost(random) {
-    const like = document.querySelector('article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button'); //seleziono il bottone like
+    const next = document.querySelector('a.coreSpriteRightPaginationArrow');//select next post button
+    const like = document.querySelector('article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button'); //select like button
     if(like){
         console.log('click\n');
         like.click();
@@ -10,26 +13,35 @@ function likePost(random) {
     console.log(random+' miliseconds\n');
 }
 
+/**
+ * Timer Trigger
+ * @param {bool} flag flag wich will let to know us about the reached time
+ */
 let time = (flag) => {
-    setTimeout(toTrue(flag),300000); //imposto il flag a true dopo 30 minuti
+    setTimeout(toTrue(flag),300000); //after 30 min, set flag to true
 }
 
+/**
+ * Set to true a flag
+ * @param {bool} flag flag wich will let to know us about the reached time
+ */
 let toTrue = (flag) => {
     flag = true;
 }
 
+//main function
 (function loop() {
-    let flag = false; //flag che ci consenmte di tracciare il timer
+    let flag = false; //initialize the flag wich will let to know us about the reached time
     time(flag);
 
-    if(flag) //se il flag è true e quindi è passata mezz'ora esco dalla funzione
+    if(flag) 
         return;
 
-    var min = 10000; // min 10 secondi
-    var max = 20000; // max 20 secondi
+    var min = 10000; // min 10 seconds
+    var max = 20000; // max 20 seconds
     var random = Math.floor(Math.random() * (+max - +min)) + +min;
     setTimeout(function() {
         likePost(random);
-        loop();  //richiamo ricorsivamente la stessa funzione
+        loop();  //recursive looping
     }, random);
 }());
